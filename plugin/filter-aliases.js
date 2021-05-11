41df9c4f8d69899a663149e795cb1420134e5397
risenforces <risenforces@gmail.com>
2021-05-11 17:22:46 +0500

    fix #30

--- plugin/filter-aliases.js
+++ plugin/filter-aliases.js
@@ -1,7 +1,15 @@
+function fromEntries(entries) {
+  const result = {}
+  for (const [key, value] of entries) {
+    result[key] = value
+  }
+  return result
+}
+
 function filterAliases(aliases, filter) {
   const entries = Object.entries(aliases)
   const filteredEntries = entries.filter(filter)
-  return Object.fromEntries(filteredEntries)
+  return fromEntries(filteredEntries)
 }
 
 exports.filterAliases = filterAliases
